bf4c7dd81d9a806472935578f3915d3805e6e865
Sebastian Muszynski <s.muszynski@constructiva.de>
2017-08-14 08:02:01 +0200

    Pull request https://github.com/rytilahti/python-mirobo/pull/35 merged for testing.

--- mirobo/__init__.py
+++ mirobo/__init__.py
@@ -1,7 +1,8 @@
 # flake8: noqa
 from mirobo.protocol import Message, Utils
-from mirobo.vacuumcontainers import VacuumStatus, ConsumableStatus, CleaningDetails, CleaningSummary, Timer
+from mirobo.containers import VacuumStatus, ConsumableStatus, CleaningDetails, CleaningSummary, Timer
 from mirobo.vacuum import Vacuum, VacuumException
 from mirobo.plug import Plug
 from mirobo.strip import Strip
+from mirobo.ceil import Ceil
 from mirobo.device import Device, DeviceException
