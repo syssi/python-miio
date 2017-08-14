bdaeeb6ce4cff939e4662cfaca7c2cc49bb19110
Sebastian Muszynski <s.muszynski@constructiva.de>
2017-08-14 21:47:12 +0200

    Latest changes of the pull request 35 merged.

--- mirobo/ceil.py
+++ mirobo/ceil.py
@@ -1,12 +1,61 @@
 from .device import Device
 from typing import Any, Dict
 
+class CeilStatus:
+    """Container for status reports from Xiaomi Philips LED Ceiling Lamp"""
+
+    def __init__(self, data: Dict[str, Any]) -> None:
+        # ['power', 'bright', 'snm', 'dv', 'cctsw', 'bl', 'mb', 'ac', 'ms'
+        #  'sw', 'cct']
+        # ['off', 0, 4, 0, [[0, 3], [0, 2], [0, 1]], 1, 1, 1, 1, 99]
+        # NOTE: Only 8 properties can be requested at the same time
+        self.data = data
+
+    @property
+    def power(self) -> str:
+        return self.data["power"]
+
+    @property
+    def is_on(self) -> bool:
+        return self.power == "on"
+
+    @property
+    def bright(self) -> int:
+        return self.data["bright"]
+
+    @property
+    def snm(self) -> int:
+        return self.data["snm"]
+
+    @property
+    def dv(self) -> int:
+        return self.data["dv"]
+
+    @property
+    def cct(self) -> int:
+        return self.data["cct"]
+
+    @property
+    def bl(self) -> int:
+        return self.data["bl"]
+
+    @property
+    def ac(self) -> int:
+        return self.data["ac"]
+
+    def __str__(self) -> str:
+        s = "<CeilStatus power=%s, bright=%s, cct=%s, snm=%s, dv=%s, " \
+            "bl=%s, ac=%, >" % \
+            (self.power, self.bright, self.cct, self.snm, self.dv,
+             self.bl, self.ac)
+        return s
+
 
 class Ceil(Device):
     """Main class representing Xiaomi Philips LED Ceiling Lamp."""
 
     # TODO: - Auto On/Off Not Supported
-    #       - Adjust Scens with Wall Switch Not Supported
+    #       - Adjust Scenes with Wall Switch Not Supported
 
     def on(self):
         """Power on."""
@@ -48,65 +97,11 @@ class Ceil(Device):
         """Auto CCT Off."""
         return self.send("enable_ac", [0])
 
-    def status(self):
+    def status(self) -> CeilStatus:
         """Retrieve properties."""
-        properties = ['power', 'bright', 'snm', 'dv', 'cct'
-                      'sw', 'bl', 'mb', 'ac', 'ms', ]
+        properties = ['power', 'bright', 'cct', 'snm', 'dv', 'bl', 'ac', ]
         values = self.send(
             "get_prop",
             properties
         )
         return CeilStatus(dict(zip(properties, values)))
-
-
-class CeilStatus:
-    """Container for status reports from Xiaomi Philips LED Ceiling Lamp"""
-
-    def __init__(self, data: Dict[str, Any]) -> None:
-        # ['power', 'bright', 'snm', 'dv', 'cctsw', 'bl', 'mb', 'ac', 'ms']
-        # ['off', 0, 4, 0, [[0, 3], [0, 2], [0, 1]], 1, 1, 1]
-        # NOTE: ms doesn't return any value
-        self.data = data
-
-    @property
-    def power(self) -> str:
-        return self.data["power"]
-
-    @property
-    def is_on(self) -> bool:
-        return self.power == "on"
-
-    @property
-    def bright(self) -> int:
-        return self.data["bright"]
-
-    @property
-    def snm(self) -> int:
-        return self.data["snm"]
-
-    @property
-    def dv(self) -> int:
-        return self.data["dv"]
-
-    @property
-    def cctsw(self) -> tuple:
-        return self.data["cctsw"]
-
-    @property
-    def bl(self) -> int:
-        return self.data["bl"]
-
-    @property
-    def mb(self) -> int:
-        return self.data["mb"]
-
-    @property
-    def ac(self) -> int:
-        return self.data["ac"]
-
-    def __str__(self) -> str:
-        s = "<CeilStatus power=%s, bright=%s, snm=%s, dv=%s, cctsw=%s " \
-            "bl=%s, mb=%s, ac=%s, >" % \
-            (self.power, self.bright, self.snm, self.dv, self.cctsw,
-             self.bl, self.mb, self.ac)
-        return s
